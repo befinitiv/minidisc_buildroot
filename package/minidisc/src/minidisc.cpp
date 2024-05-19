810ef81ca479613f50def288b8515b0bc431757b
befi <no@email.de>
2024-05-19 21:55:36 +0200

    Fixed some premature program termination issues. Still the SW structure is in a bit of a sorry state, need proper structure

--- package/minidisc/src/minidisc.cpp
+++ package/minidisc/src/minidisc.cpp
@@ -15,22 +15,26 @@ SSD1306 display;
 bool poweroff = false;
 
 void ready() {
-	std::cout << "ready" << std::endl;
-	
-	namespace fs = std::filesystem;
-	for (auto const& dir_entry : fs::recursive_directory_iterator("/tmp/cd")) {
-				if(!dir_entry.is_regular_file() || dir_entry.path().extension() != ".mp3")
-					continue;
-        std::cout << dir_entry.path().string() << '\n';
-				display.text(dir_entry.path().filename());
-				mpg123.play(dir_entry.path().string());
-				if(poweroff)
-					break;
+	std::cout << "Loading" << std::endl;
+
+	try {
+		namespace fs = std::filesystem;
+		for (auto const& dir_entry : fs::recursive_directory_iterator("/tmp/cd")) {
+					if(!dir_entry.is_regular_file() || dir_entry.path().extension() != ".mp3")
+						continue;
+					std::cout << dir_entry.path().string() << '\n';
+					display.text(dir_entry.path().filename());
+					mpg123.play(dir_entry.path().string());
+					if(poweroff)
+						break;
+		}
 	}
+	catch(...) {}
 }
 
 void notReady() {
 	std::cout << "not ready" << std::endl;
+	display.text("No disc");
 }
 
 void pressed(unsigned int num) {
@@ -60,14 +64,22 @@ void pressed(unsigned int num) {
 
 int main() {
 	display.text("Moin");
-	sleep(3);
-	Cd cd(ready, notReady);
 	Buttons buttons(pressed);
+	
+	for(;;) {
+		try {
+			Cd cd(ready, notReady);
 
-	while(!poweroff) {
-		sleep(1);
+			while(!poweroff) {
+				sleep(1);
+			}
+			buttons.powerOff();
+			poweroff = false;
+		}	 catch (const std::exception& e) {
+			display.text(e.what());
+			sleep(3);
+		}
 	}
-	buttons.powerOff();
 
 	return 0;
 }
